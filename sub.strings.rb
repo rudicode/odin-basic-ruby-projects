b9d2ff080c5e5dd6029efe2c35a3c5a19ff5d531
Rudi Luzar <rudi@rudicode.com>
2020-04-30 10:28:32 -0400

    Fixed formating.

--- sub.strings.rb
+++ sub.strings.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 # Assignment
-# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+# Implement a method #substrings that takes a word as the first argument and
+# then an array of valid substrings (your dictionary) as the second argument.
+# It should return a hash listing each substring (case insensitive) that was
+# found in the original string and how many times it was found.
 
 #  >    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 #  =>    ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
